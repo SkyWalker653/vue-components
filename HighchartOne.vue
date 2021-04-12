c9ff017c0e36a24746cbadc5d8605010a57bb349
Priya Ranjan <priyaranjan.653@gmail.com>
2021-04-12 13:52:31 +0530

    Create HighchartOne.vue

---
+++ HighchartOne.vue
@@ -0,0 +1,23 @@
+<template>
+    <highcharts :options="chartOptions"></highcharts>
+</template>
+
+<script>
+// import HighchartsVue from 'highcharts-vue'
+import { Chart } from 'https://unpkg.com/highcharts-vue@1.3.5/dist/highcharts-vue.min.js'
+
+export default {
+  data () {
+    return {
+      chartOptions: {
+        series: [{
+          data: [1, 2, 3] // sample data
+        }]
+      }
+    }
+  },
+  components: {
+    highcharts: Chart
+  }
+}
+</script>
