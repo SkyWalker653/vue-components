82ce09300c67e3038abd2180d9d7f7ef2ab783ab
Priya Ranjan <priyaranjan.653@gmail.com>
2021-05-24 20:28:17 +0530

    Create QuasarTest2.vue

---
+++ QuasarTest2.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Testing</h1>
+        <q-btn color="primary" label="Primary" />
+    </div>
+</template>
+
+<script>
+import { QBtn } from 'quasar'
+
+export default {
+  components: {
+    QBtn
+  }
+}
+</script>
